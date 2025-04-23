c1c96850a389687617fb9156580e493070e0f788
Evgeny-Git79 <evgeny.zabolotnikov@gmail.com>
2025-04-23 18:20:35 +1000

    1

--- main.py
+++ main.py
@@ -8,24 +8,26 @@ url = "https://www.divan.ru/vladivostok/category/svet"
 driver.get(url)
 time.sleep(3)
 
-vacancies = driver.find_elements(By.CSS_SELECTOR, 'div._Ud0k')
+lamps = driver.find_elements(By.CSS_SELECTOR, 'div._Ud0k')
 
 parsed_data = []
 
-for vacancy in vacancies:
+for lamp in lamps:
     try:
-        title = vacancy.find_element(By.CSS_SELECTOR, 'span.vacancy-name--SYbxrgpHgHedVTkgI_cA').text
-        company = vacancy.find_element(By.CSS_SELECTOR, 'span.company-info-text--O32pGCRW0YDmp3BHuNOP').text
-        salary = vacancy.find_element(By.CSS_SELECTOR, 'span.compensation-text--cCPBXayRjn5GuLFWhGTJ').text
-        link = vacancy.find_element(By.CSS_SELECTOR, 'a.bloko-link').get_attribute('href')
+        title = lamp.find_element(By.CSS_SELECTOR, 'span').text
+        print(title)
+        price = lamp.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU').text
+        print(price)
+        link = lamp.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+        print(link)
     except:
         print("произошла ошибка при парсинге")
         continue
-    parsed_data.append([title, company, salary, link])
+#    parsed_data.append([title, link])
 
 driver.quit()
 
-with open("lamp.csv", 'w',newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Название вакансии', 'название компании', 'зарплата', 'ссылка на вакансию'])
-    writer.writerows(parsed_data)
+# with open("lamp.csv", 'w',newline='', encoding='utf-8') as file:
+#    writer = csv.writer(file)
+#    writer.writerow(['Название лампы', 'Цена', 'Ссылка'])
+#    writer.writerows(parsed_data)
